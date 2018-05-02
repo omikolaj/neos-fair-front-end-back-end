fbf750f78a166f017cf15232b13a80274d36c200
Oski <github email address>
2018-05-02 17:34:02 -0400

    Added before_create hook in the users model to generate a new token before a new user is created

--- server-side/app/models/user.rb
+++ server-side/app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
+    before_create -> {self.token = generate_token}
     has_many :drafts, class_name: 'Draft' 
     has_many :ad_items 
     has_many :ads, :through => :ad_items
     has_many :items, :through => :ads
-    has_many :orders 
+    has_many :orders
+
+    private
+
+    def generate_token
+        loop do
+            token = SecureRandom.hex
+            return token unless User.exists?({token: token})
+        end
+    end
 end
