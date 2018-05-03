de1c4de5c1d85ad9cd35b5d4a882bf3e8eb41b1a
Oski <github email address>
2018-05-03 13:28:35 -0400

    Setup react project with necessary imports for react-dom, react-router-dom, redux, react-redux, redux-thunk

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//import reducer1 from ''
+//import reducer2 from ''
+
+const rootReducer = combineReducers({
+  red1: 'reducer1',
+  red2: 'reducer2'
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
